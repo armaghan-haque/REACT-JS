7a9e259ac3db8e7b6af779eaf159a8eef8e33166
Armaghan Haque <armaghan.haque.25@gmail.com>
2026-08-03 23:13:29 +0530

    Add initial project setup with React, Vite, and basic components
    - Created Card component to display user information
    - Added SVG assets for React and Vite logos
    - Implemented global styles in index.css
    - Set up main entry point in main.jsx
    - Configured Vite for React application

---
+++ 02-COMPONENTS/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <div className="card">
+            <h1>arman</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Card
