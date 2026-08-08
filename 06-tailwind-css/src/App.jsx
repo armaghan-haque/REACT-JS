9680c970772c210e02ec9a4e0d9444a7644e602f
Armaghan Haque <armaghan.haque.25@gmail.com>
2026-08-08 20:46:36 +0530

    Add initial setup for React with Vite and Tailwind CSS
    - Created main entry point for React application in `main.jsx`
    - Added Tailwind CSS import in `index.css`
    - Configured Vite with React and Tailwind CSS plugins in `vite.config.js`
    - Added React and Vite logos as SVG assets

---
+++ 06-tailwind-css/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <h3 class='text-3xl font-bold underline bg-amber-400'>
+          this is tailwind css
+        </h3>
+    </div>
+  )
+}
+
+export default App
