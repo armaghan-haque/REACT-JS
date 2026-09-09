e142e8d84c6ac549cbfe20ccc63331d47aefbfe7
Armaghan Haque <armaghan.haque.25@gmail.com>
2026-09-09 23:55:09 +0530

    feat: integrate Tailwind CSS and update routing structure
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Simplified the App component by removing unnecessary routes and components.
    - Updated index.css to use Tailwind CSS instead of custom styles.
    - Configured Vite to include Tailwind CSS in the build process.

--- 19-routing-advanced/src/App.jsx
+++ 19-routing-advanced/src/App.jsx
@@ -1,20 +1,9 @@
+import React from 'react'
+
 const App = () => {
   return (
-    <div className='h-screen bg-black text-white'>
-      <Navbar />
-      <Navbar2 />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/courses' element={<Courses />} />
-        <Route path='/courses/:courseId' element={<CourseDetail />} />
+    <div>App</div>
+  )
+}
 
-        <Route path='/product' element={<Product />}>
-          <Route path='men' element={<Men />} />
-          <Route path='women' element={<Women />} />
-          <Route path='kids' element={<Kids />} />
-        </Route>
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </div>
+export default App
